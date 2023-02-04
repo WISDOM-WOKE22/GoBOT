e5f6b0503dea397198d8bac0db6bff42855a2c24
Wisdom woke <wisdomwokedev@gmail.com>
2023-02-04 09:16:15 +0100

    initial commit

--- index.js
+++ index.js
@@ -10,36 +10,8 @@ app.use(express.json())
 // const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
     apiKey: "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"
-    // apiKey: "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"
-    // apiKey: "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"
 });
 const openai = new OpenAIApi(configuration);
-
-
-
-// // const  = require("openai");
-// const configuration = new Configuration({
-    // //   apiKey: "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",
-    //   apiKey: "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",
-    // });
-    // const openAi = new OpenAIApi(configuration);
-    
-    
-// app.post('/api/v1', async (request, response) => {
-//     const Response = await openAi.createCompletion({
-//         model: "text-davinci-003",
-//         prompt: "Say this is a test",
-//         max_tokens: 7,
-//         temperature: 0,
-//       })
-//     response.status(200)
-//     .json({
-    //         status:'success',
-    //         message: {
-//             Response
-//         }
-//     })
-// })
 
 app.post('/api/v1/start', async (request, response) => {
     try{
@@ -49,20 +21,6 @@ app.post('/api/v1/start', async (request, response) => {
                     max_tokens: 7,
                     temperature: 0,
                   })
-        // const Response = await openai.listModels()
-        // const Response = await openai.retrieveModel("text-davinci-003");
-        // const Response = await openai.listModels();
-    // const Response = await openai.createCompletion({
-    //     "model": "text-davinci-003",
-    //     "prompt": "Say this is a test",
-    //     "max_tokens": 7,
-    //     "temperature": 0,
-    //     "top_p": 1,
-    //     "n": 1,
-    //     "stream": false,
-    //     "logprobs": null,
-    //     "stop": ["\n"] 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
-    //   })
      const mainResponse = Response.data.choices[0].text;
         response.status(200)
         .json({
@@ -77,6 +35,13 @@ app.post('/api/v1/start', async (request, response) => {
     }
 })
 app.use(cors())
+
+app.get('/', (request, response) => {
+    response.status(200)
+    .json({
+        message: "Welcom to GoBOT API"
+    })
+})
 
 app.use('/api/v1', Completion)
 
