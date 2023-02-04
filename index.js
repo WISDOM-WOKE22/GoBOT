5024c2642bcea6f11878a6f3fca04c96d3d90b1c
Wisdom woke <99021060+WISDOM-WOKE22@users.noreply.github.com>
2023-02-04 14:40:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,34 +6,6 @@ const Completion = require('./Routes/Routes')
 const app = express();
 
 app.use(express.json())
-
-// const { Configuration, OpenAIApi } = require("openai");
-const configuration = new Configuration({
-    apiKey: "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"
-});
-const openai = new OpenAIApi(configuration);
-
-app.post('/api/v1/start', async (request, response) => {
-    try{
-        const Response = await openai.createCompletion({
-                    model: "text-davinci-003",
-                    prompt: "is this ChatGPT    ",
-                    max_tokens: 7,
-                    temperature: 0,
-                  })
-     const mainResponse = Response.data.choices[0].text;
-        response.status(200)
-        .json({
-            mainResponse
-        })
-    } catch (error){
-        console.log(error)
-        response.status(400)
-        .json({
-            message: "API not working"
-        })
-    }
-})
 app.use(cors())
 
 app.get('/', (request, response) => {
